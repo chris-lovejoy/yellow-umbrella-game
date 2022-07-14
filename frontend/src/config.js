e02972dc13452bfcf26d47fe8efc41945d3087f1
ChrisLovejoy <35599865+chris-lovejoy@users.noreply.github.com>
2022-07-14 18:47:44 +0100

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,6 +3,6 @@
   export const selectedNetwork = 80001 // 42261
 
   // Select appropriate corresponding deployment address
-  export const geneticNFTAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3" // Oasis address
-  // export const geneticNFTAddress = "0x6A366F9f070dA4DDa7b8c9f46e6ed7AE98c8b47c" // Polygon address
+  export const geneticNFTAddress = "0xB0c351Dc5C157D30f1C5a71BF3df1D3E6BD33ADE" // Updated oasis address
+  export const geneticNFTAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3" // Prev Oasis address
   
